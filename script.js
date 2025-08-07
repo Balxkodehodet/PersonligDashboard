772f47b102d5c94c556fbb35a64cc02acf89a1ba
Balxkodehodet <balx042025@gmail.com>
2025-08-07 14:54:24 +0200

    updates

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+// initialize variables:
 let dashboard = document.getElementById("dashboard");
 let inputTxt = document.getElementById("dash-inputtxt");
+let bookList = [];
+
+//Get local storage:
+let storedData = JSON.parse(localStorage.getItem("BookList"));
+dashboard.addEventListener("submit", (e) => {
+  e.preventDefault();
+  bookList.push(inputTxt.value);
+  localStorage.setItem("BookList", JSON.stringify(bookList));
+  console.log(bookList);
+  console.log(inputTxt.value, "was stored");
+});
