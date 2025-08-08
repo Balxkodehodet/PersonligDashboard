5e101d8da1ede809b201a9038575d017ea05578f
Balxkodehodet <balx042025@gmail.com>
2025-08-09 01:28:06 +0200

    changed book title description etc from a div to inputs

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ bookList = loadData("bookList");
 
 // Hvis array er tomt og gjort om til undefined eller null av loadData
 if (Array.isArray(bookList) !== true) {
-  bookList = [];
+  bookList = []; // gjør det om til tomt array igjen
 }
 
 function renderPage() {
@@ -22,10 +22,79 @@ function renderPage() {
     bookListArea.removeChild(bookListArea.firstChild);
   }
   bookList.forEach((book) => {
+    // Create elements that are used on each book
     let bookDiv = document.createElement("div");
+    let bookForm = document.createElement("form");
+    let bookTextTitle = document.createElement("input");
+    let bookTextDesc = document.createElement("input");
+    let bookTextGenre = document.createElement("input");
+    let bookTextPages = document.createElement("input");
+    bookTextTitle.type = "text";
+    bookTextDesc.type = "text";
+    bookTextGenre.type = "text";
+    bookTextPages.type = "text";
+    let bookImg = document.createElement("img");
+    let bookEditBtn = document.createElement("button");
+    let bookFavBtn = document.createElement("button");
+
+    bookEditBtn.textContent = "Edit";
+    bookFavBtn.textContent = "Favourite";
+
+    bookImg.src = "images/book.jpg";
+    bookImg.alt =
+      "A huge book opened by a hand with a view of a cloudy forest behind it";
     bookDiv.classList.add("bookdiv");
-    bookDiv.textContent = `bok tittel: ${book.title}. Beskrivelse: ${book.description}. Sjanger: ${book.sjanger}. Antall sider: ${book.pages}`;
+    bookEditBtn.classList.add("btn");
+    bookFavBtn.classList.add("btn");
+    bookTextTitle.classList.add("txtarea");
+    bookTextDesc.classList.add("txtarea");
+    bookTextGenre.classList.add("txtarea");
+    bookTextPages.classList.add("txtarea");
+    bookForm.classList.add("inputform");
+
+    let bookTitle = `Bok tittel: ${book.title}.\n`;
+    let bookDesc = `Beskrivelse: ${book.description}.\n`;
+    let bookGenre = `Sjanger: ${book.sjanger}.\n`;
+    let bookPages = `Antall sider: ${book.pages}.\n`;
+    // Append image to the left of the text
+    bookDiv.append(bookImg);
+    // Append title, description, genre, and pages with br between
+    //bookTextArea.readOnly = true;
+    bookTextTitle.value = bookTitle;
+    bookTextDesc.value = bookDesc;
+    bookTextGenre.value = bookGenre;
+    bookTextPages.value = bookPages;
+    // bookTextArea.appendChild(document.createElement("br"));
+    // bookTextArea.appendChild(bookDesc);
+    // bookTextArea.appendChild(document.createElement("br"));
+    // bookTextArea.appendChild(bookGenre);
+    // bookTextArea.appendChild(document.createElement("br"));
+    // bookTextArea.appendChild(bookPages);
+    bookForm.append(bookTextTitle, bookTextDesc, bookTextGenre, bookTextPages);
+    bookDiv.append(bookForm);
+    bookDiv.appendChild(bookEditBtn);
+    bookDiv.appendChild(bookFavBtn);
     bookListArea.append(bookDiv);
+
+    // Click button event listener
+    bookEditBtn.addEventListener("click", () => {
+      // Change button name
+      bookEditBtn.textContent =
+        bookEditBtn.textContent === "Update"
+          ? (bookEditBtn.textContent = "Edit")
+          : (bookEditBtn.textContent = "Update");
+      // create text area to change title
+      // let bookTxtform = document.createElement("form");
+      // let bookTxtArea = document.createElement("textarea");
+      // let bookTxtSubmit = document.createElement("input");
+      // bookTxtSubmit.type = "submit";
+      // bookTxtform.append(bookTxtArea, bookTxtSubmit);
+      // bookDiv.append(bookTxtform);
+
+      bookDiv.readOnly = !bookDiv.readOnly;
+      //bookDiv.textContent = bookDiv.value;
+      bookDiv.focus();
+    });
   });
 }
 renderPage();
