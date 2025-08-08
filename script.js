36c15b3a7448c59e8717eed328583d56f1786354
Balxkodehodet <balx042025@gmail.com>
2025-08-08 12:46:57 +0200

    added functions for localstorage and worked on push

--- script.js
+++ script.js
@@ -2,13 +2,34 @@
 let dashboard = document.getElementById("dashboard");
 let inputTxt = document.getElementById("dash-inputtxt");
 let bookList = [];
+let bookListArea = document.getElementById("bookList");
+localStorage.removeItem("bookList");
 
-//Get local storage:
-let storedData = JSON.parse(localStorage.getItem("BookList"));
+bookList = loadData("bookList");
+if (!Array.isArray(bookList)) {
+  bookList = [];
+}
 dashboard.addEventListener("submit", (e) => {
   e.preventDefault();
-  bookList.push(inputTxt.value);
-  localStorage.setItem("BookList", JSON.stringify(bookList));
+  bookList.push({
+    id: bookList.length,
+    title: inputTxt.value,
+    description: "This is a book",
+  });
+  saveData("bookList", bookList);
   console.log(bookList);
   console.log(inputTxt.value, "was stored");
+  bookList = bookList
+    .map((book) => `${book.title} is the book you wrote in`)
+    .join("\n");
+  bookListArea.append(bookList);
 });
+
+function saveData(objName, objContent) {
+  return localStorage.setItem(objName, JSON.stringify(objContent));
+}
+
+function loadData(dataToLoad) {
+  //Get local storage:
+  return JSON.parse(localStorage.getItem(dataToLoad));
+}
